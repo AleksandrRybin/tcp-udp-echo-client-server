cf11ecc4754493d9cd5dbaee157ecfad3a8da1b7
AleksandrRybin <vfibyrf007@gmail.com>
2019-05-04 17:57:53 +0300

    fix numbers search

--- inc/numbers.hpp
+++ inc/numbers.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <list>
+#include <regex>
 
-class Digits {
+class Numbers {
 
 public:
 
-    static Digits find_digits(char* str, int len);
+    static Numbers find_numbers(const char* str, int len);
 
-    void operator()(char c);
+    void operator()(std::csub_match match);
     
     void sort(bool asc);
     void print();
@@ -16,11 +17,12 @@ public:
     int get_max() const;
     int get_min() const;
     int get_sum() const;
-    const std::list<int>& get_digits() const;
+    int size() const;
+    const std::list<int>& get_numbers() const;
 
 private:
 
-    std::list<int> digits;
+    std::list<int> numbers;
     int sum = 0;
     int max = 0;
     int min = 9;
---
+++ src/numbers.cpp
@@ -0,0 +1,74 @@
+#include "numbers.hpp"
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <regex>
+
+void Numbers::operator()(std::csub_match match) {
+    int number = atoi(match.str().c_str());
+
+    if (number >= 0 && number <= 9) {
+        sum += number;
+
+        numbers.push_back(number);
+
+        if (number > max) {
+            max = number;
+        }
+
+        if (number < min) {
+            min = number;
+        }
+    }
+}
+
+Numbers Numbers::find_numbers(const char* str, int len) {
+    std::regex number("[+-]?[1-9]\\d*");
+    std::cregex_token_iterator it(str, str + len, number);
+    std::cregex_token_iterator it_end;
+
+    return std::for_each(it, it_end, Numbers());
+}
+
+void Numbers::sort(bool asc) {
+    if (asc) {
+        numbers.sort();
+    }
+    else {
+        numbers.sort(std::greater<int>());
+    }
+}
+
+void Numbers::print() {
+    if (size()) {
+        std::for_each(numbers.begin(), numbers.end(), [](const int& number){std::cout << number << " ";});
+        std::cout << std::endl 
+        << "quantity: " << size() << "sum: " << sum << " max: " << max << " min: " << min 
+        << std::endl;
+    }
+    else {
+        std::cout << "Numbers [0-9] not found" << std::endl;
+    }
+}
+
+int Numbers::get_max() const {
+    return max;
+}
+
+int Numbers::get_min() const {
+    return min;
+}
+
+int Numbers::get_sum() const {
+    return sum;
+}
+
+int Numbers::size() const {
+    return numbers.size();
+}
+
+const std::list<int>& Numbers::get_numbers() const {
+    return numbers;
+}
